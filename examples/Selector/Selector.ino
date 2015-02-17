ae863520439d08c2debca844aef68798a538cd77
Nick Robinson <nickthecoder@gmail.com>
2015-02-17 13:04:00 +0000

    Added line decoder 138 to the Selector example.

--- examples/Selector/Selector.ino
+++ examples/Selector/Selector.ino
@@ -1,26 +1,28 @@
 #include <abstractIO.h>
 #include <abstract_shift595.h>
 #include <abstract_shift164.h>
+#include <abstract_lineDecoder138.h>
 
 /*
 Use a shift registor to act as a chip-selector.
 i.e. give it an address (the chip number to enable) in the range 0..7, and the corresponding output of the shift register will be high.
 All other outputs will be low.
 
-In this example, we will drive two shift registers simultaenously, a 595 and a 164. 
-You can wire up one or both chips. I recommend hanging LEDs + resistors on the outputs, so you can see the results.
+In this example, we will drive two shift registers, a 595 and a 164, and also a line decoder 138.
+You can wire up one or more chips. If you hang LEDs + resistors on the outputs, you can see the results.
 
-The 164 doesn't have a latch, which makes it
-more applicable in most cases, as you need one less connection. Without the latch, intermediate outputs will be seen on its output's,
-but this is useually not a problem when used as a chip selector.
-The 595 should show not show any intermediate outputs.
+The 164 only uses 2 pins, which makes it more applicable in most cases.
+However, as it doesn't have a latch, intermediate outputs will be seen on its output's, but this is usually not a problem when used as a chip selector.
+The 595 and the 138 should not show any intermediate outputs.
+If you need 4 or fewer outputs, then the the 138 will only need 1 or 2 pins.
 
 NOTE. The same code can be used to chain multiple 595s or 164s together, just chain the end of one register to the data-in of the next,
-and change the values of "lines" as appropriate.
+and change the values of "lines" as appropriate. Currently the code for the 138 doesn't support chaining.
 */
 
 Shift595Selector *selector595;
 Shift164Selector *selector164;
+LineDecoder138 *selector138;
 
 int lines = 8; // Number of output lines of the selector.
 
@@ -33,6 +35,7 @@ void setup()
     Serial.begin( 9600 );
     selector595 = new Shift595Selector( 12, 13, 11, lines ); // latch, clock, data, output line count
     selector164 = new Shift164Selector( 2, 3, lines ); // clock, data, output line count
+    selector138 = new LineDecoder138( 4, 5, 6 ); // Address pins
 }
 
 void loop()
@@ -41,12 +44,14 @@ void loop()
     for ( int i = 0; i < lines - 1; i ++ ) {
         selector595->select( i );
         selector164->select( i );
+        selector138->select( i );
         delay( 500 );
     }
     Serial.println( "Down" );
     for ( int i = lines - 1; i > 0; i -- ) {
         selector595->select( i );
         selector164->select( i );
+        selector138->select( i );
         delay( 500 );
     }
 }
